5ab154e3ca8c8add8c259d68095334f92e39dfb6
Kenneth <kennethkskristensen@hotmail.com>
2021-02-18 15:37:21 +0100

    changed css

--- script/showXML.js
+++ script/showXML.js
@@ -2,7 +2,7 @@ let test = function() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-         document.getElementById("innerSection2").innerHTML = xhttp.responseText;
+         document.getElementById("innerSection2").innerHTML += xhttp.responseText;
          console.log(xhttp.responseText);
         }
     }
